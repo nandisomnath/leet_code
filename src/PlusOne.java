707de29b27aa93fc13cd2ccbc971f377ba80a525
nandisomnath <somnathnandi368@gmail.com>
2025-07-09 13:15:36 +0530

    plus one solved all test case passes and 0ms runtime

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -3,26 +3,28 @@ import java.util.Arrays;
 public class PlusOne {
 
     public int[] plusOne(int[] digits) {
-        int[] finalArray = new int[digits.length+1];
+        int[] finalArray = new int[digits.length + 1];
         finalArray[0] = 0;
         int borrow = 0;
         int result = 0;
 
-
-        for (int i = digits.length-1; i >= 0 ; i--) {
+        for (int i = digits.length - 1; i >= 0; i--) {
             int curr = digits[i];
-            if (i == digits.length-1) {
+            if (i == digits.length - 1) {
                 result = curr + 1;
             } else {
-                result = curr;
+                result = curr + finalArray[i + 1];
             }
-            
+
             if (result >= 10) {
                 borrow = 1;
-                result = result % 10;
+                result = 0;
+
+                finalArray[i + 1] = result;
+                finalArray[i] = borrow;
+            } else {
+                finalArray[i + 1] = result;
             }
-            finalArray[i+1] = result;
-            finalArray[i] = finalArray[i] + borrow;
         }
 
         if (finalArray[0] == 0) {
@@ -32,10 +34,9 @@ public class PlusOne {
         return finalArray;
     }
 
-
     public static void main(String[] args) {
-        int[] digits = {9};
-        
+        int[] digits = { 9, 9 };
+
         digits = new PlusOne().plusOne(digits);
 
         System.out.println(Arrays.toString(digits));
